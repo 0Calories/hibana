29cd625e5572dc01e6d164a24e169d90a104684c
Ash Anand <cppanand@gmail.com>
2025-12-24 13:21:59 -0500

    add proxy utils for token refresh

---
+++ utils/supabase/proxy.ts
@@ -0,0 +1,43 @@
+import { createServerClient } from '@supabase/ssr';
+import { type NextRequest, NextResponse } from 'next/server';
+
+export async function updateSession(request: NextRequest) {
+  const { NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY } =
+    process.env;
+
+  if (!NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || !NEXT_PUBLIC_SUPABASE_URL) {
+    throw new Error('Supabase Environment variables are not configured!');
+  }
+
+  let supabaseResponse = NextResponse.next({
+    request,
+  });
+
+  const supabase = createServerClient(
+    NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options: _options }) => {
+            request.cookies.set(name, value);
+          });
+          supabaseResponse = NextResponse.next({
+            request,
+          });
+          cookiesToSet.forEach(({ name, value, options }) => {
+            supabaseResponse.cookies.set(name, value, options);
+          });
+        },
+      },
+    },
+  );
+
+  // refreshing the auth token
+  await supabase.auth.getUser();
+
+  return supabaseResponse;
+}
