8756c38cd5310a57d82b77f279997975c5d55579
Ash Anand <cppanand@gmail.com>
2025-12-26 13:14:33 -0500

    set up shadcn project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Geist, Geist_Mono, Figtree } from 'next/font/google';
 import './globals.css';
+import { cn } from "@/lib/utils";
+
+const figtree = Figtree({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className={cn("h-full", figtree.variable)}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       >
