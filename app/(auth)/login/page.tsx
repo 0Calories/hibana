5dc059e1aacf37c5623f460a1a9b7daed78923c2
Ash Anand <cppanand@gmail.com>
2025-12-28 14:12:28 -0500

    feat(auth): Add Login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,18 +1,11 @@
-import { login, signup } from './actions';
+import { LoginForm } from './form';
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login} type="submit">
-        Log in
-      </button>
-      <button formAction={signup} type="submit">
-        Sign up
-      </button>
-    </form>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
   );
 }
