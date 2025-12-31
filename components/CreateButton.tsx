ffcb03b098a7387fa82bc2c6b12f2646898d27db
Ash Anand <cppanand@gmail.com>
2025-12-31 15:31:46 -0500

    add dialog modal when new todo is clicked

--- components/CreateButton.tsx
+++ components/CreateButton.tsx
@@ -7,6 +7,16 @@ import {
 } from 'lucide-react';
 import { Button } from './ui/button';
 import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './ui/dialog';
+import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
@@ -14,33 +24,65 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from './ui/dropdown-menu';
+import { Input } from './ui/input';
+import { Label } from './ui/label';
 
 export function CreateButton() {
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button className="radius-lg size-10">
-          <PlusIcon />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="m-4 md:m-0 md:mb-2">
-        <DropdownMenuLabel>Create new</DropdownMenuLabel>
-        <DropdownMenuSeparator />
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button className="radius-lg size-10">
+            <PlusIcon />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="m-4 md:m-0 md:mb-2">
+          <DropdownMenuLabel>Create new</DropdownMenuLabel>
+          <DropdownMenuSeparator />
 
-        <CreateTodoButton />
-        <CreateNoteButton />
-        <CreateHabitButton />
-        <CreateScheduleButton />
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <CreateTodoButton />
+          <CreateNoteButton />
+          <CreateHabitButton />
+          <CreateScheduleButton />
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create a new Todo item</DialogTitle>
+          <DialogDescription>
+            Fill out the form to keep track of something you need to do
+          </DialogDescription>
+        </DialogHeader>
+
+        <div className="flex items-center gap-2">
+          <div className="grid flex-1 gap-2">
+            <Label htmlFor="link" className="sr-only">
+              Name
+            </Label>
+            <Input defaultValue="New Todo" />
+          </div>
+        </div>
+
+        <DialogFooter className="sm:justify-start">
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
 
 function CreateTodoButton() {
   return (
-    <DropdownMenuItem>
-      <ListTodoIcon /> Todo
-    </DropdownMenuItem>
+    <DialogTrigger asChild>
+      <DropdownMenuItem>
+        <ListTodoIcon /> Todo
+      </DropdownMenuItem>
+    </DialogTrigger>
   );
 }
 
