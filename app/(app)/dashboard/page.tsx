1a0d17a4a823384d1d75f532f92983e52053aeb0
Ash Anand <cppanand@gmail.com>
2026-01-01 16:51:29 -0500

    fetch todos from supabase and render on home

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,9 +1,19 @@
 import Image from 'next/image';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { createClient } from '@/utils/supabase/server';
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const supabase = await createClient();
+
+  const todos = await supabase.from('todos').select();
+  console.dir(todos);
+
   return (
     <div className="size-full p-4">
       <Image src={'/ember.png'} alt={'Ember'} width={64} height={64} />
+      {todos.data?.map((todo) => (
+        <Card key={todo.id}>{todo.content}</Card>
+      ))}
     </div>
   );
 }
--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,9 +1,10 @@
 import { createBrowserClient } from '@supabase/ssr';
 import { getSupabaseCredentials } from './env';
+import type { Database } from './types';
 
 export function createClient() {
   const { publicUrl, publishableKey } = getSupabaseCredentials();
 
   // Create a supabase client on the browser with project's credentials
-  return createBrowserClient(publicUrl, publishableKey);
+  return createBrowserClient<Database>(publicUrl, publishableKey);
 }
