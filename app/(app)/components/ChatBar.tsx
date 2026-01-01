edc76bf42c54a84ab7e62e370b65a01afd55f628
Ash Anand <cppanand@gmail.com>
2026-01-01 17:06:51 -0500

    add blur backdrop effect to chatbar

--- app/(app)/components/ChatBar.tsx
+++ app/(app)/components/ChatBar.tsx
@@ -4,7 +4,10 @@ import { InputGroup, InputGroupInput } from '@/components/ui/input-group';
 
 export function ChatBar() {
   return (
-    <InputGroup className="rounded-lg p-5 pl-2 pr-2 mr-2">
+    <InputGroup
+      id="ember-chat"
+      className="rounded-lg p-5 pl-2 pr-2 mr-2 backdrop-blur-lg"
+    >
       <InputGroupInput placeholder="Ask Ember to do something ..." />
     </InputGroup>
   );
