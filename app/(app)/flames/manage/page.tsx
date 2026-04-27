de5568266a5a78290523a30111d47b0ee20dad60
Ash Anand <cppanand@gmail.com>
2026-04-27 13:38:49 -0400

    chore: unused t in flame management page

--- app/(app)/flames/manage/page.tsx
+++ app/(app)/flames/manage/page.tsx
@@ -1,9 +1,7 @@
-import { getTranslations } from 'next-intl/server';
 import { getAllFlamesForManagement } from '../actions';
 import { ManageFlamesList } from './components/ManageFlamesList';
 
 export default async function ManageFlamesPage() {
-  const t = await getTranslations('flames.manage');
   const result = await getAllFlamesForManagement();
   const flames = result.success ? result.data : [];
 
