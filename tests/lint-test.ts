eb283a867e9c3fe02f2ee8692ba23b967ab6edaf
Ash Anand <cppanand@gmail.com>
2026-04-29 18:21:22 -0400

    Revert "test(ci): Add intentional lint and type errors to verify CI"
    This reverts commit 3439a609e085566645a86b63231cb9916d880c86.

--- tests/lint-test.ts
+++
@@ -1,7 +0,0 @@
-// Intentional lint error: unused variable
-const unusedVariable = 'this should fail lint';
-
-// Intentional type error: assigning string to number
-const count: number = 'not a number';
-
-export {};
