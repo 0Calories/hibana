b4e3872485edcb5898562940c8731ae8d0895023
Ash Anand <cppanand@gmail.com>
2026-04-29 16:28:26 -0400

    test: add boilerplate for login e2e test

---
+++ tests/login.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from '@playwright/test';
+
+test('should navigate to the login page', async ({ page }) => {
+  await page.goto('/');
+  await page.click('text=Log In');
+  await expect(page).toHaveURL('http://localhost:3000/login');
+});
